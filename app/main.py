4f6989d5e3756f849f3b9682ffbd6f2aa86ac99c
2021-a151 <2021.a151@gmail.com>
2026-01-31 17:28:48 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,3 +3,5 @@ from app.routes.health import router as health_router
 
 app = FastAPI(title="Kyotei MVP API", version="0.1")
 app.include_router(health_router, prefix="/api")
+from app.routes.dbtest import router as dbtest_router
+app.include_router(dbtest_router, prefix="/api")
