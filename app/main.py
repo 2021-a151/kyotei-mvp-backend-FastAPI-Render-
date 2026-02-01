ef89444fa2f376a6f8aa5c4a704ce82fed296f29
2021-a151 <2021.a151@gmail.com>
2026-02-01 17:07:10 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,3 +10,4 @@ def root():
 app.include_router(health_router, prefix="/api")
 from app.routes.dbtest import router as dbtest_router
 app.include_router(dbtest_router, prefix="/api")
+from app.routes.dbtest import router as dbtest_router
